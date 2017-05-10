d3c17d43cad72c62ecab2365c67101bc0e4fa487
Nikita Koval <nkoval@devexperts.com>
2017-05-10 23:58:27 +0300

    * Use license-maven-plugin * Add deployment to local repository

--- core/src/main/java/com/devexperts/aprof/hotspot/Method.java
+++ core/src/main/java/com/devexperts/aprof/hotspot/Method.java
@@ -1,5 +1,27 @@
 package com.devexperts.aprof.hotspot;
 
+/*-
+ * #%L
+ * Aprof Core
+ * %%
+ * Copyright (C) 2002 - 2017 Devexperts, LLC
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 /**
  * Method tag from hotspot compilation log. For example:
  * "&ltmethod id='729' holder='646' name='indexOf' return='634' arguments='634 634' flags='1' bytes='70' iicount='3836'/&gt;"
--- core/src/test/java/com/devexperts/aprof/FastObjIntMapTest.java
+++ core/src/test/java/com/devexperts/aprof/FastObjIntMapTest.java
@@ -1,22 +1,26 @@
-/*
- * Aprof - Java Memory Allocation Profiler
- * Copyright (C) 2002-2014  Devexperts LLC
- *
+package com.devexperts.aprof;
+
+/*-
+ * #%L
+ * Aprof Core
+ * %%
+ * Copyright (C) 2002 - 2017 Devexperts, LLC
+ * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
-
-package com.devexperts.aprof;
 
 import com.devexperts.aprof.util.FastObjIntMap;
 import junit.framework.TestCase;
